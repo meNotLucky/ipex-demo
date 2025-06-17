76b0c0a336a7227454b6ca78b90f7e6f9f68034a
Lukas Clausson <lukas.clausson@hotmail.com>
2025-06-17 19:11:50 +0200

    Added selection functionality
    - Meshes can now be selected and highlighted by clicking on them
    - Supports multiple scenes if necessary

--- ipex-demo/src/main.ts
+++ ipex-demo/src/main.ts
@@ -1,9 +1,32 @@
-import * as BAB from '@babylonjs/core';
+import * as BABYLON from '@babylonjs/core';
+import '@babylonjs/loaders';
+import * as SELECTION from './selection';
 
-const canvas = document.getElementById("render-canvas");
-const engine = new BAB.Engine(canvas);
-const scene = new BAB.Scene(engine);
+const canvas = (document.getElementById("render-canvas") as HTMLCanvasElement);
+const engine = new BABYLON.Engine(canvas, true, { stencil: true });
+
+const scene = new BABYLON.Scene(engine);
+scene.createDefaultCameraOrLight(true, false, true);
+
+const importResult = await BABYLON.ImportMeshAsync("models/combi.glb", scene);
+const sphere = BABYLON.MeshBuilder.CreateSphere("sphere");
+
+scene.onPointerObservable.add((pointerInfo) => {
+  switch (pointerInfo.type) {
+    case BABYLON.PointerEventTypes.POINTERPICK: {
+        if (pointerInfo.pickInfo == null)
+            break;
+
+        let mesh = pointerInfo.pickInfo.pickedMesh;
+        if (pointerInfo.pickInfo.hit && mesh != null)
+            SELECTION.setSelectedMesh((mesh as BABYLON.Mesh));
+    }
+      break;
+    }
+});
 
 engine.runRenderLoop(function() {
     scene.render();
 });
+
+window.addEventListener("resize", function() { engine.resize() });
---
+++ ipex-demo/src/selection.ts
@@ -0,0 +1,101 @@
+import * as BABYLON from '@babylonjs/core';
+
+/** Holds references to the selected meshes for each scene */
+let selectedMeshes = new Map<BABYLON.Scene, BABYLON.Nullable<BABYLON.Mesh>>();
+/** Holds references to the highlight layers created for each scene */
+let highlightLayers = new Map<BABYLON.Scene, BABYLON.Nullable<BABYLON.HighlightLayer>>();
+
+/** Global alpha for selection outline pulse animation */
+let selectionAnimPulseAlpha = 0;
+
+/**
+ * Selects a new mesh for the scene and highlights it, can be queried with getSelectedMesh()
+ * @param mesh The mesh to be selected, it needs to currently be in a scene
+*/
+export function setSelectedMesh(mesh: BABYLON.Nullable<BABYLON.Mesh>)
+{
+    if (mesh == null)
+        return;
+
+    let scene = mesh.getScene();
+
+    if (hasSelectedMesh(scene))
+        clearSelectedMesh(scene);
+
+    if (!hasSceneLayer(scene))
+        addNewSceneLayer(scene);
+
+    selectedMeshes.set(scene, mesh);
+    
+    highlightLayers.get(scene)?.addMesh((mesh as BABYLON.Mesh), BABYLON.Color3.Yellow());
+}
+
+/**
+ * Clears the currently selected mesh for the given scene
+ * @param scene The scene to clear mesh selection in
+ * */
+export function clearSelectedMesh(scene: BABYLON.Scene)
+{
+    if (!hasSceneLayer(scene))
+        return;
+    if (!hasSelectedMesh(scene))
+        return;
+
+    highlightLayers.get(scene)?.removeMesh((getSelectedMesh(scene) as BABYLON.Mesh));
+
+    selectedMeshes.set(scene, null);
+}
+
+/**
+ * Check if the given scene currently has a mesh selected
+ * @param scene The scene to check for mesh selection
+ * @returns true | false
+ */
+export function hasSelectedMesh(scene: BABYLON.Scene) : boolean
+{
+    return selectedMeshes.has(scene) && selectedMeshes.get(scene) != null;
+}
+
+/**
+ * Get the selected mesh for the given scene, if available
+ * @param scene The scene to get the selected mesh from
+ * @returns The selected mesh
+ */
+export function getSelectedMesh(scene: BABYLON.Scene) : BABYLON.Nullable<BABYLON.Mesh>
+{
+    if (!hasSelectedMesh(scene))
+        return null;
+
+    return (selectedMeshes.get(scene) as BABYLON.Nullable<BABYLON.Mesh>);
+}
+
+/**
+ * Checks if a scene layer has already been added
+ * @param scene The scene to check for
+ * @returns true | false
+ */
+function hasSceneLayer(scene: BABYLON.Scene) : boolean
+{
+    return selectedMeshes.has(scene) && highlightLayers.has(scene);
+}
+
+/**
+ * Adds a new scene layer
+ * @param scene The scene layer to add
+ */
+function addNewSceneLayer(scene: BABYLON.Scene)
+{
+    selectedMeshes.set(scene, null);
+
+    let sceneHighlightLayer = new BABYLON.HighlightLayer("selectionHighlight", scene);
+    sceneHighlightLayer.innerGlow = false;
+
+    scene.registerBeforeRender(() => {
+        selectionAnimPulseAlpha += 0.02;
+        const blurSize = 0.3; const blurAnimOffset = 0.2;
+        sceneHighlightLayer.blurHorizontalSize = blurSize + (Math.cos(selectionAnimPulseAlpha) * blurAnimOffset) + blurAnimOffset;
+        sceneHighlightLayer.blurVerticalSize = blurSize + (Math.cos(selectionAnimPulseAlpha) * blurAnimOffset) + blurAnimOffset;
+    });
+
+    highlightLayers.set(scene, sceneHighlightLayer);
+}
